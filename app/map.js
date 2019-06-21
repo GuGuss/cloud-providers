3639fa31f3b813a5e9ea0a001920b64af48d5d84
Augustin Delaporte <augustin@platform.sh>
2019-06-21 21:44:51 +0800

    New data source

--- app/map.js
+++ app/map.js
@@ -1,7 +1,17 @@
 import { regions } from "./regions";
 import { mapStyle } from "./mapstyle";
+import alibaba from './providers/alibaba.json';
+import aws from './providers/aws.json';
+import azure from './providers/azure.json';
+import digitalocean from './providers/digitalocean.json';
+import gc from './providers/google.json';
+import interoute from './providers/interoute.json';
+import oracle from './providers/oracle.json';
+import rackspace from './providers/rackspace.json';
+import upcloud from './providers/upcloud.json';
 
 export let map;
+export const providers = [alibaba, aws, azure, digitalocean, gc, interoute, oracle, rackspace, upcloud];
 const markers = [];
 
 function initMap() {
@@ -24,62 +34,59 @@ export function addMapToDom() {
 }
 
 function setMarkers(map) {
-  let iconPath = 'assets/images/';
-  let iconSize = '48';
-
   // Loop through all providers.
-  for (let provider in regions) {
+  for ( var i = 0; i < providers.length; i++) {
+    var provider = providers[i];
+    console.log(provider.providerId);
 
-    // Initialize markers[provider] if not yet done.
-    if (typeof markers[provider] == 'undefined') {
-      markers[provider] = [];
+    // Initialize markers[] if not yet done.
+    if (typeof markers[provider.providerId] == 'undefined') {
+      markers[provider.providerId] = [];
     }
 
-    // Loop through all regions of each provider.
-    for (let region in regions[provider]){
-      
-      const region = regions[provider][region];
+    // Generate a random color for each provider.
+    var providerColor = '#' + Math.floor(Math.random()*16777215).toString(16);
 
-      if (region['open'] !== false) {
-        // @TODO: Don't display the marker if 'open: false'.
-        // @TODO: Group markers together if located at the same place.
-        const marker = new google.maps.Marker({
-          position: {
-            lat: region['lat'],
-            lng: region['lng']
-          },
-          map: map,
-          title: region['name'],
-          icon: iconPath + 'map-marker-' + provider + '-' + iconSize + '.png',
-          label: region['zones']
-          }
-        );
+    // Loop through all regions.
+    for ( var x = 0; x < provider.regions.length; x++) {
+      var region = provider.regions[x];
 
-        // Add the new marker to markers[provider].
-        markers[provider].push(marker);
+      const marker = new google.maps.Marker({
+        position: {
+          lat: region.locationLat,
+          lng: region.locationLong
+        },
+        map: map,
+        title: region.providerCode,
+        icon: colorMarker(providerColor)
+      });
 
-        // Display an info popup which opens when the marker is clicked.
-        const popupString = '<div id="content">'+
-                '<h3 id="firstHeading" class="firstHeading">' + region['name'] + ' (' + provider + ')</h3>'+
-                '<div id="bodyContent">' +
-                  '<ul>' + 
-                    '<li>ID: <b>' +  region['id'] + '</b></li>' +
-                    '<li>Location: <b>' +  region['location'] + '</b></li>' +
-                    '<li>Availability Zones: <b>' +  region['zones'] + '</b></li>' +
-                  '</ul>' + 
-                  '<a href="#" target="_blank">See the list of all ' + provider + ' regions.</a>'+
-                '</div>'+
-              '</div>';
+      // Add the new marker to markers[].
+      markers[provider.providerId].push(marker);
 
-        const infowindow = new google.maps.InfoWindow({
-          content: popupString,
-          maxWidth: 400
-        });
+      // Display an info popup which opens when the marker is clicked.
+      const popupString = '<div id="content">'+
+              '<h3 id="firstHeading" class="firstHeading">' + region.providerCode + ' (' + provider.shortName + ')</h3>'+
+              '<div id="bodyContent">' +
+                '<ul>' + 
+                  '<li>Location: <b>' + region.city + ', ' + region.country + '</b></li>' +
+                  '<li>Data Centers: <b>' + region.facilities + '</b></li>' +
+                  '<li>Code: <b>' + region.providerCode + '</b></li>' +
+                  '<li>Notes: <b>' + region.notes + '</b></li>' +
+                  '<li>SLA: <b>' + provider.sla + '% (<a href="' + provider.slaLink + '" target="_blank">link</a>)</b></li>' +
+                '</ul>' + 
+                '<a href="' + provider.website + '" target="_blank">Access ' + provider.providerId + ' website.</a>'+
+              '</div>'+
+            '</div>';
 
-        marker.addListener('click', function () {
-          infowindow.open(map, marker);
-        });
-      }
+      const infowindow = new google.maps.InfoWindow({
+        content: popupString,
+        maxWidth: 400
+      });
+
+      marker.addListener('click', function () {
+        infowindow.open(map, marker);
+      });
     }
   }
 }
@@ -91,6 +98,7 @@ function setStyle(map) {
 
 // Sets the map on all markers in the array.
 export function setMapOnAll(map, provider) {
+  console.log(markers[provider]);
   for (let i = 0; i < markers[provider].length; i++) {
     markers[provider][i].setMap(map);
   }
@@ -104,4 +112,16 @@ function clearMarkers(provider) {
 // Shows any markers currently in the array.
 function showMarkers(provider) {
   setMapOnAll(map, provider);
+}
+
+// Set a specific color for each marker.
+function colorMarker(providerColor) {
+  return {
+    path: 'M 0,0 C -2,-20 -10,-22 -10,-30 A 10,10 0 1,1 10,-30 C 10,-22 2,-20 0,0 z M -2,-30 a 2,2 0 1,1 4,0 2,2 0 1,1 -4,0',
+    fillColor: providerColor,
+    fillOpacity: 1,
+    strokeColor: '#000',
+    strokeWeight: 2,
+    scale: 1,
+  };
 }
